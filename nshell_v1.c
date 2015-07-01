c12ae9ea2db9221815bc58a6d7ddb92ba7811438
Shyam Prasad Sitaula <sitaula.s@ic685b3593c709.nunet.neu.edu>
2015-06-30 22:05:40 -0400

    june-30

--- nshell_v1.c
+++ nshell_v1.c
@@ -1,11 +1,6 @@
-/*
-  nshell_v1.c
-  Last modified: sps, 6-29-2015
-*/
-/*
-  Source code of a command line interpreter.
-  ------------------------------------------
-*/
+/*nshell_v1.c, Last modified: sps, 6-30-2015*/
+/*C code of a linux command line interpreter*/
+
 /*Headers*/
 #include<stdio.h>
 #include<string.h>
@@ -58,7 +53,6 @@ int main(int argc, char **argv)
 	shell_tester();
 	return 0;
 }
-
 /*Shell tester*/
 void shell_tester(void)
 {
@@ -70,23 +64,21 @@ void shell(void)
 {
 	int i;
 	char s[200];
-	int previous_command_background = 0;
-	int previous_command_subcommands = 0;
-	
 	struct Command command;
+	/*infinite*/
 	for(;;)
 	{
 		/*Read a string from user*/
 		printf("nshell $  ");
 		fgets(s, sizeof(s), stdin);
 		s[strlen(s)-1] = '\0';
-		/*Read the command*/
+		/*Read the subcommands, redirections and bakground*/
 		ReadCommand(s, &command);
 		read_rd_bg(&command);
-		//PrintCommand(&command);
-		/*For more than one subcommand*/
-		if(command.num_sub_commands > 1)
+		/*If at least one command*/
+		if(command.num_sub_commands > 0)
 		{
+			/*Create pipes*/
 			int *pipe_arr = malloc((command.num_sub_commands-1) * 2 * sizeof(int));
 			int *pipe_arr_current = pipe_arr;
 			for(i=0; i<command.num_sub_commands-1; i++)	
@@ -95,20 +87,23 @@ void shell(void)
 				pipe_arr_current += 2;
 			}
 			pipe_arr_current = pipe_arr;
-			/*Now that we have all the pipes*/
+			/*Create the array that will store child pids*/
 			int *child_pid_arr = malloc(command.num_sub_commands * sizeof(int));
 			int *child_pid_current = child_pid_arr;
+			/*Fork() a child for each subcommand*/
 			for(i=0; i<command.num_sub_commands; i++)
 			{
 				child_pid_current[i] = fork();
 				if(child_pid_current[i] == -1)
 					exit(1);
+				/*Child executes the command*/
 				else if(child_pid_current[i] == 0)
 				{
 					child_do_work(i, &command, pipe_arr);
 					i = command.num_sub_commands;	
 					exit(1);
 				}
+				/*Parent closes the pipe*/
 				else if(child_pid_current[i] > 0)
 				{
 					if(i > 0)
@@ -119,47 +114,18 @@ void shell(void)
 					}
 				}
 			}
+			/*If background operator given*/
 			if(command.background ==1)
 			{
 				printf("[%d]\n", child_pid_arr[command.num_sub_commands-1]);
-				previous_command_background = 1;
-				previous_command_subcommands = command.num_sub_commands;
 			}
+			/*If background operator not given*/
 			else if(command.background == 0)
 			{
 				int status;
-				previous_command_background = 0;
-				previous_command_subcommands = 0;
 				for(i=0; i<command.num_sub_commands; i++)
 				{
 					waitpid(child_pid_arr[i], &status, WUNTRACED | WCONTINUED);
-				}
-			}
-		}
-		/*If only one subcommand*/
-		else if(command.num_sub_commands == 1)
-		{
-			int dummy = 1;
-			int child_pid = fork();
-			if(child_pid == -1)
-				exit(1);
-			else if(child_pid == 0)
-				child_do_work(-1, &command, &dummy);
-			else if(child_pid > 0)
-			{
-				if(command.background == 1)
-				{
-					printf("[%d]\n", child_pid);
-					//previous_command_background = 1;
-					//previous_command_subcommands = command.num_sub_commands;
-				}
-				else if(command.background == 0)
-				{
-					int status;
-					previous_command_background = 0;
-					previous_command_subcommands = 0;
-					//wait(NULL);
-					waitpid(child_pid, &status, WUNTRACED | WCONTINUED);
 				}
 			}
 		}
@@ -172,10 +138,7 @@ void shell(void)
 	/*Shell complete*/
 	return;
 }
-
-/*This function will take a string 
-  and extract arguments (Just like how main() 
-  does with command line arguments)*/
+/*This function takes  a string and extracts arguments separeted by spaces.*/
 void read_args(char *in, char **argv, int max_args)
 {
 	int argc;
@@ -205,8 +168,7 @@ void read_args(char *in, char **argv, int max_args)
 	}
 	return;
 }
-/*PRINT*/
-/*This function prints the arguments*/
+/*This function prints the arguments of each subcommand*/
 void print_args(char **argv)
 {
 	int i = 0;
@@ -217,8 +179,6 @@ void print_args(char **argv)
 	}
 	return;
 }
-
-
 /*Read the Command from the line (get/extract all the SubCommands)*/
 void ReadCommand(char *s, struct Command *command)
 {
@@ -238,7 +198,6 @@ void ReadCommand(char *s, struct Command *command)
 			break;
 	}
 	command->num_sub_commands = i;
-	
 	/*Call read_args() to get the tokens for each sub-command*/
 	for(i=0; i<command->num_sub_commands; i++)
 	{
@@ -246,8 +205,7 @@ void ReadCommand(char *s, struct Command *command)
 	}
 	return;
 }
-/*Print the command (all subcommands)*/
-/*For debugging*/
+/*Print the command (all subcommands)*//*For debugging*/
 void PrintCommand(struct Command *command)
 {
 	if(command->num_sub_commands == 0)
@@ -270,87 +228,27 @@ void PrintCommand(struct Command *command)
 	printf("\n");
 	return;
 }
-
 /*Reads redirections and background operator*/
 void read_rd_bg(struct Command *command)
 {
-	/*Variables needed in this function*/
 	int i;
 	struct SubCommand *sub_command_current = NULL;
 	char **argv_current = NULL;
-
 	/*Initialize*/
 	command->stdin_redirect = NULL;
 	command->stdout_redirect = NULL;
 	command->background = 0;
-
 	int how_many_subcommands = command->num_sub_commands;
-
-	/*Three cases*/
-	/*No subcommand*/
+	/*Two cases*/
+	/*0 subcommand*/
 	if(how_many_subcommands == 0)
 		return;
-
-	/*Only one subcommand*/
-	else if (how_many_subcommands == 1)
-	{
-		/*Get the only subcommand*/		
-		sub_command_current = &command->sub_commands[how_many_subcommands-1];
-		argv_current = sub_command_current->argv;
-		/*Find out how many tokens in this sub_command*/
-		i = 0;
-		while(argv_current[i] != NULL)
-			i++;
-		/*Check for the background operator*/
-		if(strcmp(argv_current[i-1], "&") == 0)
-		{
-			if(i != 1)
-			{
-				command->background = 1;
-				argv_current[i-1] = NULL;
-			}
-		}
-		/* Now check for first redirect operator*/
-		i = 0;
-		while(argv_current[i] != NULL)
-			i++;
-		if(i > 1 && strcmp(argv_current[i-2], ">") == 0)
-		{
-			command->stdout_redirect = strdup(argv_current[i-1]);
-			argv_current[i-1] = NULL;
-			argv_current[i-2] = NULL;
-		}
-		else if(i > 1 && strcmp(argv_current[i-2], "<") == 0)
-		{
-			command->stdin_redirect = strdup(argv_current[i-1]);
-			argv_current[i-1] = NULL;
-			argv_current[i-2] = NULL;
-		}
-		/* Now check for second redirect operator*/
-		i = 0;
-		while(argv_current[i] != NULL)
-			i++;
-		if(i > 1 && strcmp(argv_current[i-2], ">") == 0)
-		{
-			command->stdout_redirect = strdup(argv_current[i-1]);
-			argv_current[i-1] = NULL;
-			argv_current[i-2] = NULL;
-		}
-		else if(i > 1 && strcmp(argv_current[i-2], "<") == 0)
-		{
-			command->stdin_redirect = strdup(argv_current[i-1]);
-			argv_current[i-1] = NULL;
-			argv_current[i-2] = NULL;
-		}
-	}
-
-	/*2 or more subcommands*/
-	else if (how_many_subcommands >= 2)
+	/*At least one subcommands*/
+	else if (how_many_subcommands >= 1)
 	{
 		/*Find the last subcommand*/	
 		sub_command_current = &command->sub_commands[how_many_subcommands-1];
 		argv_current = sub_command_current->argv;
-		/*Find out how many tokens in this sub_command*/
 		i = 0;
 		while(argv_current[i] != NULL)
 			i++;
@@ -376,11 +274,10 @@ void read_rd_bg(struct Command *command)
 		/*Find the first subcommand*/
 		sub_command_current = &command->sub_commands[0];
 		argv_current = sub_command_current->argv;
-		/*Find out how many tokens in this sub_command*/
 		i = 0;
 		while(argv_current[i] != NULL)
 			i++;
-		/*Now check for input redirector*/
+		/*Check for input redirector*/
 		i = 0;
 		while(argv_current[i] != NULL)
 			i++;
@@ -391,57 +288,11 @@ void read_rd_bg(struct Command *command)
 			argv_current[i-2] = NULL;
 		}
 	}
-
 	return;
 }
-
 /*This function is executed by child processes*/
 void child_do_work(int i, struct Command *command, int *pipe_fds)
 {
-	/*If the only subcommand*/
-	if(i == -1)
-	{
-	 	if(command->num_sub_commands > 0)
-		{
-			/*Do redirection if needed*/
-			/*input*/
-			if(command->stdin_redirect != NULL)
-			{
-				close(0);
-				int fd = open(command->stdin_redirect, O_RDONLY);
-				if(fd == -1)
-				{
-					fprintf(stderr, "Error: %s : Could not open file\n", command->stdin_redirect);
-					exit(1);
-				}
-			}
-			/*Output*/
-			if(command->stdout_redirect != NULL)
-			{
-				close(1);
-				int fd = open(command->stdout_redirect, O_WRONLY | O_CREAT | O_TRUNC, 0660);
-				if(fd == -1)
-				{
-					fprintf(stderr, "Error: %s : Could not open file\n", command->stdout_redirect);
-					exit(1);
-				}
-
-			}
-
-			/*Execute the subcommand*/
-			int check_exec = execvp(command->sub_commands[0].argv[0], command->sub_commands[0].argv);
-			if (check_exec == -1)
-			{
-				fprintf(stderr, "%s: Command not found\n", command->sub_commands[0].argv[0]);
-				exit(2);
-			}
-		}
-		else
-			exit(0);
-	}
-	/*Not the only subcommand*/
-	else
-	{
 		/*Write to the pipe*/
 		if(i < command->num_sub_commands-1)
 		{
@@ -481,7 +332,6 @@ void child_do_work(int i, struct Command *command, int *pipe_fds)
 				fprintf(stderr, "Error: %s : Could not open file\n", command->stdout_redirect);
 				exit(1);
 			}
-
 		}
 		/*Execute*/
 		int check_exec = execvp(command->sub_commands[i].argv[0], command->sub_commands[i].argv);
@@ -490,6 +340,6 @@ void child_do_work(int i, struct Command *command, int *pipe_fds)
 			fprintf(stderr, "%s: Command not found\n", command->sub_commands[i].argv[0]);
 			exit(2);
 		}
-	}
 	return;
 }
+/*---End, line no: 345---*/
